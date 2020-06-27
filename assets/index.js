505e221b193c273fcc1f1b0037ab41e44ed1ae34
Juan David <gottygarcia@gmail.com>
2020-06-26 23:38:12 -0500

    Primer plugin añadido

---
+++ assets/index.js
@@ -0,0 +1,10 @@
+import MediaPlayer from './MediaPlayer.js'
+import AutoPlay from './plugins/Autoplay.js'
+
+const video = document.querySelector('#MediaPlayer__video')
+const button_play_pause = document.querySelector('#btn-play-pause')
+const button_mute_inmute = document.querySelector('#btn-mute-inmute')
+
+const player = new MediaPlayer({ el: video, plugins: [new AutoPlay()] })
+button_play_pause.onclick = () => player.togglePlay()
+button_mute_inmute.onclick = () => player.toggleMute()
