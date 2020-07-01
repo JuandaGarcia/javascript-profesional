79de462d6f5786f0c0219c3d0fd1bc85b21988c2
Juan David <gottygarcia@gmail.com>
2020-07-01 00:58:52 -0500

    intersectionObserver, visibilityChange, ServiceWorkers

--- assets/index.js
+++ assets/index.js
@@ -1,13 +1,28 @@
 import MediaPlayer from './MediaPlayer.js'
-import AutoPlay from './plugins/Autoplay.js'
+import AutoPlay from './plugins/AutoPlay.js'
+import AutoPause from './plugins/AutoPause.js'
 
 const video = document.querySelector('#MediaPlayer__video')
-const button_play_pause = document.querySelector('#btn-play-pause')
-const button_mute_inmute = document.querySelector('#btn-mute-inmute')
-
 const player = new MediaPlayer({
 	el: video,
-	plugins: [new AutoPlay()],
+	plugins: [new AutoPlay(), new AutoPause()],
 })
-button_play_pause.onclick = () => player.togglePlay()
-button_mute_inmute.onclick = () => player.toggleMute()
+
+const playButton = document.querySelector('#btn-play-pause')
+playButton.onclick = () => player.togglePlay()
+
+const muteButton = document.querySelector('#btn-mute-inmute')
+muteButton.onclick = () => {
+	if (player.media.muted) {
+		player.unmute()
+	} else {
+		player.mute()
+	}
+}
+
+//service worker
+// if ('serviceWorker' in navigator) {
+// 	navigator.serviceWorker.register('/sw.js').catch((error) => {
+// 		console.log(error.message)
+// 	})
+// }
